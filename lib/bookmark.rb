89b53fe198b27d5eaae6ea5ab75ab6e90e2448f3
zidehen <zoeidehen@gmail.com>
2021-12-09 23:52:18 +0000

    step 12-protected app against SQL injection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -30,7 +30,7 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
     
-    result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title")
+    result = connection.exec_params("INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title])
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
