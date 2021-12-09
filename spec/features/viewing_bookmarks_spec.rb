46238e130ab76af65d96ab005d646a2ba6078902
zidehen <zoeidehen@gmail.com>
2021-12-09 12:30:50 +0000

    refactored dummy data insertion in feature_spec viewing bookmarks

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,9 +1,8 @@
 feature 'Viewing bookmarks' do
   scenario 'see a list of bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com/');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com/');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    Bookmark.create(url: "http://www.makersacademy.com/")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com/")
     
     visit('/bookmarks')
 
