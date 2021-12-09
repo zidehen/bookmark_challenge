772740c53258b7ba701327bde133ec74a4527351
zidehen <zoeidehen@gmail.com>
2021-12-09 15:14:50 +0000

    step 11, updated add.erb view and create post route with title

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/bookmarks' do
-    Bookmark.create(params[:url])
+    Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
 
