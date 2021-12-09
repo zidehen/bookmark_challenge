8204570cc6d6f0dbbd3a9f9501182dd819c9ad61
zidehen <zoeidehen@gmail.com>
2021-12-09 15:05:31 +0000

    step 11, updated create bookmark with failing feature test

---
+++ db/migrations/02_add_title_to_bookmarks.sql
@@ -0,0 +1,1 @@
+ALTER TABLE bookmarks ADD title VARCHAR(60);
--- spec/features/create_bookmark_spec.rb
+++ spec/features/create_bookmark_spec.rb
@@ -2,7 +2,8 @@ feature 'Creating bookmarks' do
     scenario 'can add a bookmark to the list' do
       visit('/bookmarks/add')
       fill_in(:url, with: 'www.test.com')
+      fill_in(:title, with: 'Test Bookmark')
       click_button('Add')
-      expect(page).to have_content 'www.test.com'
+      expect(page).to have_link('Test Bookmark', href: 'www.test.com')
     end
 end
