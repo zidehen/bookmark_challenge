8a46bb50c9b04e15f36d9248c3dd5922d93a7281
zidehen <zoeidehen@gmail.com>
2021-12-28 16:37:25 +0000

    step 16-feature test added for invalid url

--- spec/features/create_bookmark_spec.rb
+++ spec/features/create_bookmark_spec.rb
@@ -1,9 +1,18 @@
 feature 'Creating bookmarks' do
-    scenario 'can add a bookmark to the list' do
-      visit('/bookmarks/add')
-      fill_in(:url, with: 'www.test.com')
-      fill_in(:title, with: 'Test Bookmark')
-      click_button('Add')
-      expect(page).to have_link('Test Bookmark', href: 'www.test.com')
-    end
+  scenario 'can add a bookmark to the list' do
+    visit('/bookmarks/add')
+    fill_in(:url, with: 'www.test.com')
+    fill_in(:title, with: 'Test Bookmark')
+    click_button('Add')
+    expect(page).to have_link('Test Bookmark', href: 'www.test.com')
+  end
+
+  scenario 'the bookmark must be a valid URL' do
+    visit('/bookmarks/add')
+    fill_in(:url, with: 'not a valid bookmark')
+    click_button('Add')
+
+    expect(page).not_to have_content "not a valid bookmark"
+    expect(page).to have_content "You must submit a valid URL"
+  end
 end
